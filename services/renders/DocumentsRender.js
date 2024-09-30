b1b61f335ab56997fbc0cc677872096b65732fc2
Nguyen Viet Van <nguyenvietvan2002@gmail.com>
2024-09-30 15:12:49 +0700

    update-30/9

--- services/renders/DocumentsRender.js
+++ services/renders/DocumentsRender.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { View,  TouchableOpacity, Text, Alert, Modal, TextInput } from 'react-native';
 import { Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import styles from '../../styles/style';
+import { StyleSheet } from 'react-native';
 
 export default DocumentsRender = ({item}) => {
     const filePath = "https://docs.google.com/document/d/1ZPJW1LO1gQz7JAF1SA-KcRK1npAkeqPC/view"
@@ -20,7 +20,7 @@ export default DocumentsRender = ({item}) => {
     return(
         <View style={styles.container}>
         <TouchableOpacity style={styles.attachmentContainer} onPress={handlePress}>
-             <Icon name="file" size={24} />
+             <Icon name="file" size={30} />
              <Text style={styles.fileName}>aaa</Text>
          </TouchableOpacity>
 
@@ -42,3 +42,63 @@ export default DocumentsRender = ({item}) => {
     </View> 
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    attachmentContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
+    fileName: {
+        marginLeft: 10,
+        fontSize: 16,
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 60,
+        left: 5,
+        padding: 10,
+        borderRadius: 5,
+    },
+    closeButtonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+    pdf: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    navigationContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+    },
+    pageInput: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 5,
+        width: 80,
+        marginRight: 10,
+    },
+    goButton: {
+        backgroundColor: '#007BFF',
+        padding: 10,
+        borderRadius: 5,
+    },
+    goButtonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+    input: {
+        borderWidth:1,
+        borderRadius:10,
+        width:Dimensions.get('window').width,
+    }
+});
